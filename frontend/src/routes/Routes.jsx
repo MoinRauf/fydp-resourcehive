8bdb69de9fce301fc51e3db8e9f0294d45412d6a
Moin Rauf <147373573+MoinRauf@users.noreply.github.com>
2025-02-07 21:35:38 +0500

    Update Files

---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,25 @@
+// src/Route.jsx
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// Import Page Components
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+// import SignUp from "../pages/user-signup/sign-up/SignUp";
+// import SignIn from "../pages/user-signin/sign-in/SignIn";
+
+const RouteComponent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      {/* < Route path="/SignUp" element={<SignUp />} />
+     < Route path="/SignIn" element={<SignIn />} />
+      */}
+    </Routes>
+  );
+};
+
+export default RouteComponent;
